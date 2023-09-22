80fcff1f42cc1b1f489dd36882f72814aec7e869
IhorKozloff <87847488+IhorKozloff@users.noreply.github.com>
2023-09-22 19:14:43 +0300

    Delete src/build directory

--- src/build/src/types/entities/transaction.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.KnownMonths = exports.KnownTransactionCategories = exports.KnownTransactionTypes = void 0;
-exports.KnownTransactionTypes = ['income', 'expense'];
-exports.KnownTransactionCategories = ['main', 'house', 'children', 'development', 'food', 'basic', 'products', 'car', 'health', 'child care', 'household', 'education', 'leisure', 'other', 'salary'];
-exports.KnownMonths = ['all', 'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'sebptember', 'october', 'november', 'december'];
